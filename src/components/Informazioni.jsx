533c50fb417c562e78b5a4bcd5f2a9919231b63c
alessiofranchini <123971820+alessiofranchini@users.noreply.github.com>
2024-01-23 20:29:13 +0100

    modifica foto profilo

--- src/components/Informazioni.jsx
+++ src/components/Informazioni.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import foto from '../assets/img/foto.jpg';
+import foto from '../assets/img/foto.png'
 
 function Informazioni() {
     return (
